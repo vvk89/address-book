be322b1524acd8e55efc75c44ac38ab314b7fe1c
Balaji Venkatesan <balaji.venkatesan2@ing.com>
2022-06-13 00:38:57 +0200

    need to add gitgnore

--- AddressBook1/src/test/java/lu/jemmic/addressbook/view/CommandLineTest.java
+++ AddressBook1/src/test/java/lu/jemmic/addressbook/view/CommandLineTest.java
@@ -1,11 +1,14 @@
 package lu.jemmic.addressbook.view;
 
-import junit.framework.TestCase;
 import lu.jemmic.addressbook.controller.AddressBookController;
+import lu.jemmic.addressbook.model.Category;
+import lu.jemmic.addressbook.model.Contact;
+import lu.jemmic.addressbook.model.Hair;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -14,9 +17,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.Map;
 
 import static org.mockito.Mockito.when;
 
@@ -27,43 +28,69 @@ public class CommandLineTest {
     @InjectMocks
     CommandLine commandLine;
 
+    @Mock
+    Contact contact;
+
     @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
+    public static void setUpBeforeClass() {
     }
 
     @AfterClass
-    public static void tearDownAfterClass() throws Exception {
+    public static void tearDownAfterClass() {
     }
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown(){
     }
 
     @Test
-    public void testCreateContact() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        String name = "Shiva";
-        InputStream n = new ByteArrayInputStream(name.getBytes());
-        System.setIn(n);
-        String surname = "S";
-        InputStream s = new ByteArrayInputStream(surname.getBytes());
-        System.setIn(s);
-        String phone = "068767667";
-        InputStream p = new ByteArrayInputStream(phone.getBytes());
-        System.setIn(s);
-        String mail = "S@hm.com";
-        InputStream m = new ByteArrayInputStream(mail.getBytes());
-        System.setIn(m);
-        String age = "39";
-        InputStream a = new ByteArrayInputStream(age.getBytes());
-        System.setIn(a);
-        System.setIn(a);
-        System.setIn(a);
-        //when(commandLine.getStringField();
+    public void testCreateContact() {
+        String userInput = String.format("Dan%sVega%s0687687989%sdanvega@gmail.com%sy%sBlack%sCategoryFamily%s3%sy%sy",
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator());
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
         commandLine.createContact();
     }
 
+    @Test
+    public void testDisplayContactDetail() {
+        when(contact.getHair()).thenReturn(new Hair("black"));
+        when(contact.getCategory()).thenReturn(new Category());
+        commandLine.displayContactDetail(new Contact());
+    }
+
+    // ToDo powermockito for secure random need to be added.
+    @Ignore
+    public void testEditContact() {
+        String userInput = String.format("Dan%sVega%s0687687989%sdanvega@gmail.com%s32%sBlack%sCategoryFamily%s3%sy%sy",
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator());
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+        Map<AddressBookController.ContactData, String> contactDataStringMap = Map.of(AddressBookController.ContactData.NAME, "Vivek",
+                AddressBookController.ContactData.SURNAME, "V", AddressBookController.ContactData.TELEPHONE_NUMBER, "0685554341",
+                AddressBookController.ContactData.EMAIL, "B@GMAIL.COM",
+                AddressBookController.ContactData.AGE, "35", AddressBookController.ContactData.HAIR_COLOR, "BLACK", AddressBookController.ContactData.CATEGORY_TYPE, "CategoryFamily");
+        when(contact.getCategory()).thenReturn(new Category());
+        commandLine.editContact(new Contact(), contactDataStringMap);
+    }
 }
